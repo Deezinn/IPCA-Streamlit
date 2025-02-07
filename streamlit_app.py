cb838aea8a4e36d5abd8f57ab78b9f713e0a3cfe
deezinn <developer.andrelz@gmail.com>
2025-02-06 21:43:54 -0300

    testando

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.write("Tabela 118 - IPCA dessazonalizado:")
-st.write(pd.read_csv(r'/home/deezinn/Documentos/Python/Meus Projetos Pessoais/IPCA-Streamlit/data/Tabela 118 - IPCA dessazonalizado.csv', sep=',', encoding='utf-8', skiprows=1))
+
+file_path = 'data/Tabela 118 - IPCA dessazonalizado.csv'
+
+if os.path.exists(file_path):
+    try:
+        df = pd.read_csv(file_path, sep=',', encoding='utf-8', skiprows=1)
+        st.write(df)
+    except Exception as e:
+        st.error(f"Erro ao carregar o arquivo: {e}")
+else:
+    st.error("Arquivo não encontrado! Verifique o caminho e tente novamente.")
