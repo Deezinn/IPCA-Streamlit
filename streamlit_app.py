01f176b1c22e43af4fb1e3863e51f9284052a597
deezinn <developer.andrelz@gmail.com>
2025-02-06 21:40:39 -0300

    outra correção

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 
 st.write("Tabela 118 - IPCA dessazonalizado:")
-st.write(pd.read_csv(r'/home/deezinn/Documentos/Python/Meus Projetos Pessoais/IPCA-Streamlit/data/Tabela 118 - IPCA dessazonalizado.csv',sep=','))
+st.write(pd.read_csv(r'/home/deezinn/Documentos/Python/Meus Projetos Pessoais/IPCA-Streamlit/data/Tabela 118 - IPCA dessazonalizado.csv',sep=';'))
