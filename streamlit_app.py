621445a9c20a77da55fd84ee9a2ff375b3ec46a7
deezinn <developer.andrelz@gmail.com>
2025-02-06 22:08:28 -0300

    atualizando

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,9 @@ import os
 
 file_path = "data/tabela_118_ipca.csv"
 
-if os.path.exists(file_path):
-    df = pd.read_csv(file_path, sep=",", encoding="utf-8")
+df = pd.read_csv(file_path, sep=",", encoding="utf-8")
 
-    st.title("Tabela 118 - IPCA Dessazonalizado")
-    st.write("### DataFrame carregado com sucesso:")
-    st.dataframe(df)
-else:
-    st.error(f"❌ ERRO: Arquivo CSV não encontrado em {file_path}. Verifique o caminho do arquivo.")
+st.title("Tabela 118 - IPCA Dessazonalizado")
+st.dataframe(df)
 
-st.write("✅ Aplicação Streamlit rodando corretamente!")
 st.write('teste')
