4f5ea511c7e683fd432a7a818c79ac2eaec30de0
deezinn <developer.andrelz@gmail.com>
2025-02-06 21:47:38 -0300

    outro teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.write("Tabela 118 - IPCA dessazonalizado:")
 st.write(pd.read_csv(r'/home/deezinn/Documentos/Python/Meus Projetos Pessoais/IPCA-Streamlit/data/Tabela 118 - IPCA dessazonalizado.csv', sep=',', encoding='utf-8', skiprows=1))
 
-st.write('teste')
+st.write('teste') 
